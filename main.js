69f66da6f83c23939fb68babc644338f5c74234d
KENZ <KENZ.gelsoft@gmail.com>
2016-03-21 20:49:04 +0900

    アプリとしての処理を別jsファイルに分離

---
+++ main.js
@@ -0,0 +1,25 @@
+function set(id, value) {
+    document.getElementById(id).innerText = value;
+}
+function dayOfWeek(dow) {
+    switch (dow) {
+        case 0: return '日';
+        case 1: return '月';
+        case 2: return '火';
+        case 3: return '水';
+        case 4: return '木';
+        case 5: return '金';
+        case 6: return '土';
+    }
+    return null;
+}
+function main() {    
+    var today = new Date();
+    var month = today.getMonth() + 1; // 月は 0 始まり
+    set("date", month + '/' + today.getDate());
+    set('dayOfWeek', dayOfWeek(today.getDay()));
+    
+    //testJuliusDate();
+    // testDynamicalTime();
+    testEclipticalLongitude();
+}
