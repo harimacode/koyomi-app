ddcdbe7dccfb19ae4cdbf151551c8d792a9c006b
KENZ <KENZ.gelsoft@gmail.com>
2016-03-21 20:47:57 +0900

    旧暦の計算に関する処理を別jsファイルに分離

---
+++ old_calendar.js
@@ -0,0 +1,104 @@
+/*!
+ * Date オブジェクトからユリウス日を計算します。
+ * ユリウス日は浮動小数点数として表現され、
+ * 小数点以下の数字は時刻を表します。
+ * 
+ * この関数では、グレゴリオ暦による計数となるAD 1582年10月15日
+ * 以降のみを対象とします。
+ */
+function juliusDate(date) {
+    // TODO 上記値域判定
+    var year  = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day   = date.getDate();
+    var hours = date.getHours();
+    if (month < 3) {
+        month += 12;
+        --year;
+    }
+    // alert(year + "/" + month + "/" + day);
+    return Math.floor(365.25 * year)
+         + Math.floor(year / 400)
+         - Math.floor(year / 100)
+         + Math.floor(30.59 * (month - 2))
+         + day
+         + 1721088
+         + hours / 24;
+}
+
+/*!
+ * JST のユリウス日から力学時を求めます。
+ */
+function dynamicalTime(juliusDateJST) {
+    // 元とした文献と同様、協定世界時と力学時の誤差
+    // ΔTについては無視しています。
+    // 日本標準時からの時差-9時間を引いています。
+    return juliusDateJST - (9/24);
+}
+
+/*!
+ * @brief 与えられた力学時に対応する太陽黄経を計算します。
+ * 
+ * @param t 力学時
+ * @return 太陽黄経を表す浮動小数点数
+ */
+function eclipticLongitude(juliusDateDynamicalTime) {
+    //    ｔ＝（JD+0.5-2451545.0）／36525
+    var t = (juliusDateDynamicalTime + 0.5 - 2451545.0) / 36525;
+    // alert(t);
+    //           18
+    //    λsun＝Σ Ａ*ｃｏｓ（ｋ*ｔ+θ0）
+    //           n=1
+    var table = [
+        [0.0004,    31557.0,    161.0],
+        [0.0004,    29930.0,    48.0],
+        [0.0005,    2281.0,     221.0],
+        [0.0005,    155.0,      118.0],
+        [0.0006,    33718.0,    316.0],
+        [0.0007,    9038.0,     64.0],
+        [0.0007,    3035.0,     110.0],
+        [0.0007,    65929.0,    45.0],
+        [0.0013,    22519.0,    352.0],
+        [0.0015,    45038.0,    254.0],
+        [0.0018,    445267.0,   208.0],
+        [0.0018,    19.0,       159.0],
+        [0.0020,    32964.0,    158.0],
+        [0.0200,    71998.1,    265.1],
+        [-0.0048*t, 35999.05,   267.52],
+        [1.9147,    35999.05,   267.52],
+        [36000.7695*t,  0,  0],
+        [280.4659,      0,  0],
+    ];
+    var rv = 0;
+    table.forEach(function (params) {
+        var a = params[0];
+        var k = params[1];
+        var theta0 = params[2];
+        var angle = normalizeAngle(k * t + theta0);
+        rv += a * Math.cos(angle * Math.PI / 180.0); // 毎回計算するのが遅ければ Math.PI/180.0 は定数化する
+    });
+    return normalizeAngle(rv);
+}
+function normalizeAngle(angle) {
+    while (angle < 0) {
+        angle += 360;
+    }
+    while (angle >= 360) {
+        angle -= 360;
+    }
+    return angle;
+}
+function testDynamicalTime() {
+    // 1994年5月1日0時 = 2449472.625
+    alert(2449472.625 == dynamicalTime(juliusDate(new Date(1994,4,1))));
+    // 1994年11月 8日 16:00(JST)
+    alert(2449664.2916666665 == dynamicalTime(juliusDate(new Date(1994,10,8,16,00))));
+}
+function testEclipticalLongitude() {
+    // 1994年11月8日 16:00(JST)
+    alert(225.64569002960798 == eclipticLongitude(dynamicalTime(juliusDate(new Date(1994,10,8,16,00)))));
+}
+function testJuliusDate() {
+    // 1994年5月1日 ＝ 2449473
+    alert(2449473==juliusDate(new Date(1994,4,1))); // Date#month は 0 origin
+}
