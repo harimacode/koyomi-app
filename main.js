97cb915af854ad43b7aa08d06a4ddd5cef8b0bc9
KENZ <KENZ.gelsoft@gmail.com>
2016-03-23 01:42:30 +0900

    曜日表示を単純化

--- main.js
+++ main.js
@@ -2,16 +2,7 @@ function set(id, value) {
     document.getElementById(id).innerText = value;
 }
 function dayOfWeek(dow) {
-    switch (dow) {
-        case 0: return '日';
-        case 1: return '月';
-        case 2: return '火';
-        case 3: return '水';
-        case 4: return '木';
-        case 5: return '金';
-        case 6: return '土';
-    }
-    return null;
+    return "日月火水木金土".charAt(dow);
 }
 function main() {    
     var today = new Date();
