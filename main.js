1f85f86bd6dbacc6145bdac9f9ba1f429d58a3ed
KENZ <KENZ.gelsoft@gmail.com>
2016-03-26 19:40:14 +0900

    月日だけ目立たせる調整

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function set(id, value) {
-    document.getElementById(id).innerText = value;
+    document.getElementById(id).innerHTML = value;
 }
 function dayOfWeek(dow) {
     return "日月火水木金土".charAt(dow);
@@ -7,7 +7,7 @@ function dayOfWeek(dow) {
 function main() {    
     var today = new Date(); //new Date(2014,3,16);
     var month = today.getMonth() + 1; // 月は 0 始まり
-    set("date", month + '月' + today.getDate() + '日(' + dayOfWeek(today.getDay()) + ')');
+    set("date", '<span class="month">' + month + '</span>月<span class="day">' + today.getDate() + '</span>日(' + dayOfWeek(today.getDay()) + ')');
     var jd  = juliusDate(today);
     var old = oldCalendar(jd);
     set('oldDate', '旧暦 ' + old);
