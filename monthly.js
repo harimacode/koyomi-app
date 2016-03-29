e4239a4831c827ed8d780d36e33426bf145254c5
KENZ <KENZ.gelsoft@gmail.com>
2016-03-30 07:23:48 +0900

    二回新月・満月があるとき計算できていないが仮

--- monthly.js
+++ monthly.js
@@ -1,8 +1,23 @@
+function lastDay(y, m) {
+    var nextDay = new Date(y, m + 1);
+    var rv = new Date();
+    rv.setTime(nextDay.getTime() - 24 * 60 * 60 * 1000);
+    return rv;
+}
+
 function MonthlyCalendar(year, month) {
     this.year = year;
     this.month = month;
 
     var date = new Date(year, month);
+    var last = lastDay(year, month);
+    this.newmoon = [fromJuliusDate(findSaku(juliusDate(date))),
+                    fromJuliusDate(findSaku(juliusDate(last)))];
+    var bou = findBou(juliusDate(last));
+    this.fullmoon = [fromJuliusDate(bou[0]),
+                     fromJuliusDate(bou[1])];
+    // alert(this.newmoon + "\n" + this.fullmoon);
+
     var weeks = [];
     var week = [];
     var dow = date.getDay();
@@ -30,6 +45,15 @@ MonthlyCalendar.forDate = function (date) {
     return new MonthlyCalendar(date.getFullYear(), date.getMonth());
 };
 MonthlyCalendar.prototype = {
+    isSameDay: function (a, b) {
+        // alert(a + ", " + b);
+        if (a == null || b == null) {
+            return false;
+        }
+        return a.getFullYear() == b.getFullYear() &&
+            a.getMonth() == b.getMonth() &&
+            a.getDate() == b.getDate();
+    },
     render: function () {
         // TODO: もうちょっとモダンな実現方法にしたいな…
         var s = '<tr>';
@@ -38,16 +62,31 @@ MonthlyCalendar.prototype = {
             s += '<th>' + dow.charAt(i) + '</th>';
         }
         s += '</tr>';
+        var that = this;
         this.weeks.forEach(function (week) {
             s += '<tr>';
             week.forEach(function (day) {
-                s += '<td><span class="date">';
+                var clz = "date";
+                if (that.isSameDay(day, that.fullmoon[0])) {
+                    clz += " fullmoon";
+                }
+                if (that.isSameDay(day, that.fullmoon[1])) {
+                    clz += " fullmoon";
+                }
+                if (that.isSameDay(day, that.newmoon[0])) {
+                    clz += " newmoon";
+                }
+                if (that.isSameDay(day, that.newmoon[1])) {
+                    clz += " newmoon";
+                }
+                s += '<td><div class="' + clz + '">';
                 if (day) {
+                // alert(clz);
                     s += day.getDate();
                 } else {
                     s += "";
                 }
-                s += '</span></td></a>';
+                s += '</div></td></a>';
             });
             s += '</tr>'
         });
