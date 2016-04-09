057fc177e61f2e60f9e5478cea9edf55380f6fdc
KENZ <KENZ.gelsoft@gmail.com>
2016-04-10 00:47:43 +0900

    Revert "名前空間を汚さないよう function () でラップ。"
    This reverts commit e7cfb8e33dab0569f0381ac47b90b34b01de4402.

--- main.js
+++ main.js
@@ -1,130 +1,129 @@
-(function () {
-    function set(id, value) {
-        document.getElementById(id).innerHTML = value;
-    }
-    function dayOfWeek(dow) {
-        return "日月火水木金土".charAt(dow);
-    }
-    function update() {
-        // 月表示の更新
-        var month = date.getMonth() + 1;
-        var yearMonth = [date.getFullYear(), month].join("/"); 
-        document.getElementById("gotoMonth").innerHTML = '<a href="month.html#' + yearMonth + '">&lt; '+ month +'月</a>';
-        
-        var month = date.getMonth() + 1; // 月は 0 始まり
-        var dateString = "";
-        dateString += '<span class="month">' + month + '</span>月';
-        dateString += '<span class="day">' + date.getDate() + '</span>日';
-        dateString += '<span class="dayOfWeek' + date.getDay() + '">(' + dayOfWeek(date.getDay()) + ')</span>';
-        set("date", dateString);
-        var jd  = juliusDate(date);
-        var old = oldCalendar(jd);
-        set('oldDate', '旧暦 ' + old);
-        set('rokki', rokki(old));
-        var newEto = eto(jd);
-        set('eto', newEto);
-        set('kyusei', kyusei(jd));
-        var newChoku = choku(jd);
-        set('choku', newChoku);
-        var newShuku = shuku(old);
-        set('shuku', newShuku);
-        set('nattin', nattin(jd));
-        set('nijuShisekki', nijuShisekki(jd) + "　");
-        
-        // 今解説に表がある項目について、マーカー表示します。
-        markItems([newEto.charAt(0), newEto.charAt(1), // 十干、十二支
-            newChoku, newShuku]); // 直、宿
-    }
+function set(id, value) {
+    document.getElementById(id).innerHTML = value;
+}
+function dayOfWeek(dow) {
+    return "日月火水木金土".charAt(dow);
+}
+function update() {
+    // 月表示の更新
+    var month = date.getMonth() + 1;
+    var yearMonth = [date.getFullYear(), month].join("/"); 
+    document.getElementById("gotoMonth").innerHTML = '<a href="month.html#' + yearMonth + '">&lt; '+ month +'月</a>';
+    
+    var month = date.getMonth() + 1; // 月は 0 始まり
+    var dateString = "";
+    dateString += '<span class="month">' + month + '</span>月';
+    dateString += '<span class="day">' + date.getDate() + '</span>日';
+    dateString += '<span class="dayOfWeek' + date.getDay() + '">(' + dayOfWeek(date.getDay()) + ')</span>';
+    set("date", dateString);
+    var jd  = juliusDate(date);
+    var old = oldCalendar(jd);
+    set('oldDate', '旧暦 ' + old);
+    set('rokki', rokki(old));
+    var newEto = eto(jd);
+    set('eto', newEto);
+    set('kyusei', kyusei(jd));
+    var newChoku = choku(jd);
+    set('choku', newChoku);
+    var newShuku = shuku(old);
+    set('shuku', newShuku);
+    set('nattin', nattin(jd));
+    set('nijuShisekki', nijuShisekki(jd) + "　");
+    
+    // 今解説に表がある項目について、マーカー表示します。
+    markItems([newEto.charAt(0), newEto.charAt(1), // 十干、十二支
+        newChoku, newShuku]); // 直、宿
+}
 
-    var marked;
-    function markItems(toBeMarked) {
-        if (marked) {
-            marked.forEach(function (item) {
-                removeClass(item, "marked");
-            });
+var marked;
+function markItems(toBeMarked) {
+    if (marked) {
+        marked.forEach(function (item) {
+            removeClass(item, "marked");
+        });
+    }
+    marked = [];
+    toBeMarked.forEach(function (item) {
+        var e = document.getElementById(item);
+        addClass(e, "marked");
+        marked.push(e);
+    });
+}
+
+var date;
+function today() {
+    date = new Date();
+    update();
+}
+function next(e) {
+    gotoDaysAfter(1);
+    e.preventDefault();
+}
+function prev(e) {
+    gotoDaysAfter(-1);
+    e.preventDefault();
+}
+function gotoDaysAfter(n) {
+    var ms = date.getTime();
+    ms += n * 24 * 60 * 60 * 1000;
+    date.setTime(ms);
+    update();
+}
+function gotoDate(s) {
+    date = new Date(s);
+    update();
+} 
+function addClass(e, cls) {
+    var a = e.getAttribute("class");
+    var classes = a ? a.split(' ') : [];
+    classes.push(cls);
+    e.setAttribute("class", classes.join(' '));
+}
+function removeClass(e, cls) {
+    var newClasses = [];
+    var a = e.getAttribute("class");
+    var classes = a ? a.split(' ') : [];
+    classes.forEach(function (c) {
+        if (c != cls) {
+            newClasses.push(c);
         }
-        marked = [];
-        toBeMarked.forEach(function (item) {
-            var e = document.getElementById(item);
-            addClass(e, "marked");
-            marked.push(e);
-        });
+    });
+    e.setAttribute("class", newClasses.join(' '));
+}
+window.addEventListener("load", function (e) {
+    var hash = parseHash(document.location.href);
+    if (hash) {
+        gotoDate(hash);
+    } else {
+        today();
     }
-
-    var date;
-    function today() {
-        date = new Date();
-        update();
+    
+    document.getElementById("today").addEventListener("click", today);
+    document.getElementById("next").addEventListener("click", next);
+    document.getElementById("prev").addEventListener("click", prev);
+    
+    // runTests();
+}, false);
+window.addEventListener("hashchange", function (e) {
+    var oldHash = parseHash(e.oldURL);
+    if (oldHash) {
+        var oldBox = document.getElementById(oldHash);
+        if (oldBox) {
+            removeClass(oldBox, "marked");
+        }
     }
-    function next(e) {
-        gotoDaysAfter(1);
-        e.preventDefault();
-    }
-    function prev(e) {
-        gotoDaysAfter(-1);
-        e.preventDefault();
-    }
-    function gotoDaysAfter(n) {
-        var ms = date.getTime();
-        ms += n * 24 * 60 * 60 * 1000;
-        date.setTime(ms);
-        update();
-    }
-    function gotoDate(s) {
-        date = new Date(s);
-        update();
-    } 
-    function addClass(e, cls) {
-        var a = e.getAttribute("class");
-        var classes = a ? a.split(' ') : [];
-        classes.push(cls);
-        e.setAttribute("class", classes.join(' '));
-    }
-    function removeClass(e, cls) {
-        var newClasses = [];
-        var a = e.getAttribute("class");
-        var classes = a ? a.split(' ') : [];
-        classes.forEach(function (c) {
-            if (c != cls) {
-                newClasses.push(c);
-            }
-        });
-        e.setAttribute("class", newClasses.join(' '));
-    }
-    window.addEventListener("load", function (e) {
-        var hash = parseHash(document.location.href);
-        if (hash) {
-            gotoDate(hash);
+    var newHash = parseHash(e.newURL);
+    if (newHash) {
+        if (newHash.indexOf("/") > -1) {
+            // 日付指定
+            gotoDate(newHash);
         } else {
-            today();
-        }
-        
-        document.getElementById("today").addEventListener("click", today);
-        document.getElementById("next").addEventListener("click", next);
-        document.getElementById("prev").addEventListener("click", prev);
-        
-        // runTests();
-    }, false);
-    window.addEventListener("hashchange", function (e) {
-        var oldHash = parseHash(e.oldURL);
-        if (oldHash) {
-            var oldBox = document.getElementById(oldHash);
-            if (oldBox) {
-                removeClass(oldBox, "marked");
+            // マーク
+            var newBox = document.getElementById(newHash);
+            if (newBox) {
+                addClass(newBox, "marked");
             }
         }
-        var newHash = parseHash(e.newURL);
-        if (newHash) {
-            if (newHash.indexOf("/") > -1) {
-                // 日付指定
-                gotoDate(newHash);
-            } else {
-                // マーク
-                var newBox = document.getElementById(newHash);
-                if (newBox) {
-                    addClass(newBox, "marked");
-                }
-            }
-        }
-    }, false);
-})();
+    }
+}, false);
+
--- monthly.js
+++ monthly.js
@@ -1,186 +1,184 @@
-(function () {
-    // FIXME:
-    // http://www.hottatakeshi.com/moon.html
-    // のうち、以下の日付だけ正しく満月・新月が計算できていない
-    // 2007/09/27
-    // 2010/01/01
+// FIXME:
+// http://www.hottatakeshi.com/moon.html
+// のうち、以下の日付だけ正しく満月・新月が計算できていない
+// 2007/09/27
+// 2010/01/01
 
-    function lastDay(y, m) {
-        var nextDay = new Date(y, m + 1);
-        var rv = new Date();
-        rv.setTime(nextDay.getTime() - 24 * 60 * 60 * 1000);
-        return rv;
-    }
+function lastDay(y, m) {
+    var nextDay = new Date(y, m + 1);
+    var rv = new Date();
+    rv.setTime(nextDay.getTime() - 24 * 60 * 60 * 1000);
+    return rv;
+}
 
-    function MonthlyCalendar(year, month) {
-        this.year = year;
-        this.month = month;
+function MonthlyCalendar(year, month) {
+    this.year = year;
+    this.month = month;
 
-        var date = new Date(year, month);
-        var jd = juliusDate(lastDay(year, month));
-        var newmoons = [];
-        findSakus(jd).forEach(function (saku) {
-            newmoons.push(fromJuliusDate(saku));
-        });
-        this.newmoons = newmoons;
-        var fullmoons = [];
-        findBous(jd).forEach(function (bou) {
-            fullmoons.push(fromJuliusDate(bou));
-        });
-        // alert(fullmoons.join("\n"));
-        this.fullmoons = fullmoons;
-
-        var weeks = [];
-        var week = [];
-        var dow = date.getDay();
-        for (var i = 0; i < dow; ++i) {
-            week.push(null); // 空欄
-        }
-        var month = date.getMonth();
-        do {
-            week.push(date);
-            var kMSPerOneDay = 24 * 60 * 60 * 1000;
-            var newDate = new Date();
-            newDate.setTime(date.getTime() + kMSPerOneDay);
-            date = newDate;
-            if (date.getDay() == 0) {
-                weeks.push(week);
-                week = [];
-            }
-        } while (date.getMonth() == month);
-        weeks.push(week);
-
-        this.weeks = weeks;
-    }
-
-    MonthlyCalendar.forDate = function (date) {
-        return new MonthlyCalendar(date.getFullYear(), date.getMonth());
-    };
-    MonthlyCalendar.prototype = {
-        isSameDay: function (a, b) {
-            // alert(a + ", " + b);
-            if (a == null || b == null) {
-                return false;
-            }
-            return a.getFullYear() == b.getFullYear() &&
-                a.getMonth() == b.getMonth() &&
-                a.getDate() == b.getDate();
-        },
-        render: function () {
-            // TODO: もうちょっとモダンな実現方法にしたいな…
-            var s = '<div class="week">';
-            var dow = "日月火水木金土";
-            for (var i = 0; i < dow.length; ++i) {
-                s += '<div class="dayOfWeek">' + dow.charAt(i) + '</div>';
-            }
-            s += '</div>';
-            var that = this;
-            this.weeks.forEach(function (week) {
-                s += '<div class="week">';
-                week.forEach(function (day) {
-                    var clz = "dateContainer";
-                    var date = "";
-                    var link = "";
-                    if (day) {
-                        that.fullmoons.forEach(function (fullmoon) {
-                            if (that.isSameDay(day, fullmoon)) {
-                                clz += " fullmoon";
-                            }
-                        });
-                        that.newmoons.forEach(function (newmoon) {
-                            if (that.isSameDay(day, newmoon)) {
-                                clz += " newmoon";
-                            }
-                        });
-                        date = day.getDate();
-                        var dateString = [that.year, that.month + 1, date].join('/');
-                        link = 'href="index.html#' + dateString + '"';
-                    }
-                    s += '<a class="' + clz + '" ' + link + '><div class="date">';
-                    s += date;
-                    s += '</div></a>';
-                });
-                s += '</div>'
-            });
-            return s;
-        },
-    }
-
-    function prev(e) {
-        var y = month.year;
-        var m = month.month - 1;
-        if (m < 0) {
-            --y;
-            m += 12;
-        }
-        month = new MonthlyCalendar(y, m);
-        update();
-        e.preventDefault();
-    }
-
-    function next(e) {
-        var y = month.year;
-        var m = month.month + 1;
-        if (m > 11) {
-            ++y;
-            m -= 12;
-        }
-        month = new MonthlyCalendar(y, m);
-        update();
-        e.preventDefault();
-    }
-
-    function heiseiYear(y) {
-        return "平成" + (y - 1988) + "年";
-    }
-    function jaMonth(m) {
-        var kJaNames = [
-            "睦月",
-            "如月",
-            "弥生",
-            "卯月",
-            "皐月",
-            "水無月",
-            "文月",
-            "葉月",
-            "長月",
-            "神無月",
-            "霜月",
-            "師走",
-        ];
-        return kJaNames[m];
-    }
-
-    function update() {
-        document.getElementById("current").innerHTML = '<span class="year">' + month.year + '</span>年<span class="month">' + (month.month+1) + '</span>月';
-        document.getElementById("jaYearMonth").innerHTML = heiseiYear(month.year) + " " + jaMonth(month.month);
-        document.getElementById("monthlyCalendar").innerHTML = month.render();
-    }
-
-    var month;
-    function gotoMonthOfHash() {
-        var date = new Date();
-        var hash = parseHash(document.location.href);
-        if (hash) {
-            var comps = hash.split("/");
-            while (comps.length < 3) {
-                comps.push("1");
-            }
-            date = new Date(comps.join("/"));
-        }
-        month = MonthlyCalendar.forDate(date);
-        update();
-    }
-    window.addEventListener('load', function () {
-        gotoMonthOfHash();
-
-        document.getElementById("next").addEventListener("click", next);
-        document.getElementById("prev").addEventListener("click", prev);
-    }, false);
-    window.addEventListener("hashchange", function (e) {
-        var hash = parseHash(e.newURL);
-        if (hash) {
-            gotoMonthOfHash();
-        }
+    var date = new Date(year, month);
+    var jd = juliusDate(lastDay(year, month));
+    var newmoons = [];
+    findSakus(jd).forEach(function (saku) {
+        newmoons.push(fromJuliusDate(saku));
     });
-})();
+    this.newmoons = newmoons;
+    var fullmoons = [];
+    findBous(jd).forEach(function (bou) {
+        fullmoons.push(fromJuliusDate(bou));
+    });
+    // alert(fullmoons.join("\n"));
+    this.fullmoons = fullmoons;
+
+    var weeks = [];
+    var week = [];
+    var dow = date.getDay();
+    for (var i = 0; i < dow; ++i) {
+        week.push(null); // 空欄
+    }
+    var month = date.getMonth();
+    do {
+        week.push(date);
+        var kMSPerOneDay = 24 * 60 * 60 * 1000;
+        var newDate = new Date();
+        newDate.setTime(date.getTime() + kMSPerOneDay);
+        date = newDate;
+        if (date.getDay() == 0) {
+            weeks.push(week);
+            week = [];
+        }
+    } while (date.getMonth() == month);
+    weeks.push(week);
+
+    this.weeks = weeks;
+}
+
+MonthlyCalendar.forDate = function (date) {
+    return new MonthlyCalendar(date.getFullYear(), date.getMonth());
+};
+MonthlyCalendar.prototype = {
+    isSameDay: function (a, b) {
+        // alert(a + ", " + b);
+        if (a == null || b == null) {
+            return false;
+        }
+        return a.getFullYear() == b.getFullYear() &&
+            a.getMonth() == b.getMonth() &&
+            a.getDate() == b.getDate();
+    },
+    render: function () {
+        // TODO: もうちょっとモダンな実現方法にしたいな…
+        var s = '<div class="week">';
+        var dow = "日月火水木金土";
+        for (var i = 0; i < dow.length; ++i) {
+            s += '<div class="dayOfWeek">' + dow.charAt(i) + '</div>';
+        }
+        s += '</div>';
+        var that = this;
+        this.weeks.forEach(function (week) {
+            s += '<div class="week">';
+            week.forEach(function (day) {
+                var clz = "dateContainer";
+                var date = "";
+                var link = "";
+                if (day) {
+                    that.fullmoons.forEach(function (fullmoon) {
+                        if (that.isSameDay(day, fullmoon)) {
+                            clz += " fullmoon";
+                        }
+                    });
+                    that.newmoons.forEach(function (newmoon) {
+                        if (that.isSameDay(day, newmoon)) {
+                            clz += " newmoon";
+                        }
+                    });
+                    date = day.getDate();
+                    var dateString = [that.year, that.month + 1, date].join('/');
+                    link = 'href="index.html#' + dateString + '"';
+                }
+                s += '<a class="' + clz + '" ' + link + '><div class="date">';
+                s += date;
+                s += '</div></a>';
+            });
+            s += '</div>'
+        });
+        return s;
+    },
+}
+
+function prev(e) {
+    var y = month.year;
+    var m = month.month - 1;
+    if (m < 0) {
+        --y;
+        m += 12;
+    }
+    month = new MonthlyCalendar(y, m);
+    update();
+    e.preventDefault();
+}
+
+function next(e) {
+    var y = month.year;
+    var m = month.month + 1;
+    if (m > 11) {
+        ++y;
+        m -= 12;
+    }
+    month = new MonthlyCalendar(y, m);
+    update();
+    e.preventDefault();
+}
+
+function heiseiYear(y) {
+    return "平成" + (y - 1988) + "年";
+}
+function jaMonth(m) {
+    var kJaNames = [
+        "睦月",
+        "如月",
+        "弥生",
+        "卯月",
+        "皐月",
+        "水無月",
+        "文月",
+        "葉月",
+        "長月",
+        "神無月",
+        "霜月",
+        "師走",
+    ];
+    return kJaNames[m];
+}
+
+function update() {
+    document.getElementById("current").innerHTML = '<span class="year">' + month.year + '</span>年<span class="month">' + (month.month+1) + '</span>月';
+    document.getElementById("jaYearMonth").innerHTML = heiseiYear(month.year) + " " + jaMonth(month.month);
+    document.getElementById("monthlyCalendar").innerHTML = month.render();
+}
+
+var month;
+function gotoMonthOfHash() {
+    var date = new Date();
+    var hash = parseHash(document.location.href);
+    if (hash) {
+        var comps = hash.split("/");
+        while (comps.length < 3) {
+            comps.push("1");
+        }
+        date = new Date(comps.join("/"));
+    }
+    month = MonthlyCalendar.forDate(date);
+    update();
+}
+window.addEventListener('load', function () {
+    gotoMonthOfHash();
+
+    document.getElementById("next").addEventListener("click", next);
+    document.getElementById("prev").addEventListener("click", prev);
+}, false);
+window.addEventListener("hashchange", function (e) {
+    var hash = parseHash(e.newURL);
+    if (hash) {
+        gotoMonthOfHash();
+    }
+});
