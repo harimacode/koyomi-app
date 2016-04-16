69c465fc55ecca48bc58f37dc39872b84183c847
KENZ <KENZ.gelsoft@gmail.com>
2016-04-16 16:42:07 +0900

    querySelectorAll().forEach()はなかったのでエラーになっていたのを修正

--- monthly.js
+++ monthly.js
@@ -148,7 +148,7 @@
     window.addEventListener('load', function () {
         gotoMonthOfHash();
 
-        document.querySelectorAll(".dateContainer").forEach(function (link) {
+        Array.prototype.forEach.call(document.querySelectorAll(".dateContainer"), function (link) {
             link.addEventListener("click", function (e) {
                 e.preventDefault();
                 window.location = link.getAttribute("href");
