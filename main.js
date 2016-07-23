b71f859b70bc93ba507ad089a9ab2ba569d6e360
KENZ <KENZ.gelsoft@gmail.com>
2016-07-24 04:25:32 +0900

    babelをwebpackからかけるよう変更

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 var oc = require("./harima-koyomi/old_calendar.js");
 var common = require("./common.js");
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
     output: {
         path: __dirname + '/dist',
         filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react']
+                }
+            }
+        ]
     }
 };
