abbc70add54795872032ff78326f9a814e17d970
KENZ <KENZ.gelsoft@gmail.com>
2016-07-24 16:10:24 +0900

    イベントパラメータが伝達できていなかったのを修正

--- Button.jsx
+++ Button.jsx
@@ -1,8 +1,8 @@
 var React = require("react");
 
 module.exports = React.createClass({
-    handleClick: function () {
-        this.props.onClick();
+    handleClick: function (e) {
+        this.props.onClick(e);
     },
     render: function () {
         return (
