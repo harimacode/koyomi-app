4418cd1cdfddecd011ed8f64144cd4731a94d702
KENZ <KENZ.gelsoft@gmail.com>
2021-08-24 00:46:23 +0900

    copyright -> 2021

--- CopyrightBox.jsx
+++ CopyrightBox.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default class CopyrightBox extends React.Component {
     render() {
         return (
-            <address>&copy; 2016 東京☆播磨陰陽道研究会<br />(窓際組)</address>
+            <address>&copy; 2021 東京☆播磨陰陽道研究会<br />(窓際組)</address>
         );
     }
 }
